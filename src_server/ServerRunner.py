fb907412252b1473a78d88d161bb213b03b9ee71
Eden Hamilton <eden@edenhamilton.com>
2024-01-28 07:27:03 +0000

    Added script to run both server processes at once

---
+++ src_server/ServerRunner.py
@@ -0,0 +1,7 @@
+import subprocess
+
+# Run script1.py from folder abc
+subprocess.Popen(['python', 'TCP_Relay/Relay.py'])
+
+# Run script2.py from folder plq
+subprocess.Popen(['python', 'TCP_GameHandler/Handler.py'])
